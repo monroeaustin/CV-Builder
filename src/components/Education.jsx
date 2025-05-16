83ebfc94b1db5f91a2a0184c16b710f8c8b9495f
Monroe Austin <monroeraustin2@yahoo.com>
2025-05-15 20:14:58 -0700

    feat: Update education section UI and styles - Icons rotate on hover - Sizing of Font Enhanced for UI experience

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,7 +1,18 @@
+import { IoIosArrowDown } from "react-icons/io";
+import { FaGraduationCap } from "react-icons/fa";
+
 export default function EducationSection() {
   return (
     <div className="education-section">
-      <button>Education > s </button>
+      <button className="education-button">
+        <div className="button-content">
+          <div className="button-left">
+            <FaGraduationCap className="button-icon" />
+            <h3>Education</h3>
+          </div>
+          <IoIosArrowDown className="button-icon" />
+        </div>
+      </button>
     </div>
-  )
+  );
 }
